89f5cc2244b3a1b0793003231a2bab3519c6e547
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-03-07 00:23:18 -0500

    debugging, stopping for now

--- USACO.java
+++ USACO.java
@@ -6,6 +6,7 @@ public class USACO{
 
   public static int bronze(String filename) throws FileNotFoundException{
     int[][] grid;
+    int depth = 0;
     //TODO: change to isntructions before end of lab
     int[][] stompDig; // instructions, im lazy maybe will chang before its due
     int R,C,E,N;
@@ -34,62 +35,86 @@ public class USACO{
       }
     }
 
-    //in.close();
+    in.close();
 
     for(int[] i : stompDig){
-      int[] elev = new int[9];
-      int count = 0;
-      for(int x = -1; x < 2; x++){
-          elev[count] = grid[i[0] + x][i[1] + x];
-          count++;
-      }
+      int r = i[0]-1;
+      int c = i[1]-1;
+      int stomp = i[2];
+      int max = 0;
 
-      insertionSort(elev);
-
-      for (int g = 0; g < i[2]; g++){
-        for(int f = -1; f < 2; f++){
-          if (grid[i[0] + f][i[1] + f] == elev[8] - g) {
-            grid[i[0] + f][i[1] + f]--;
+      int[] elevations = new int[9];
+      for(int b = 0; b < 9; b++){
+        for (int y = 0; y<3;y++) {
+          for (int z = 0; z<3; z++) {
+            elevations[b] = grid[r + y][c + z];
           }
         }
       }
-    }
 
-    System.out.println("");
-    for (int y = 0; y < R; y++){
-      for (int x = 0; x < C; x++){
-        System.out.print(grid[y][x] + " ");
+
+
+      System.out.println(elevations[8]);
+      max = elevations[8];
+/*
+      for (int y = 0; y<3;y++) {
+        for (int z = 0; z<3; z++) {
+          if (max < grid[r+y][c+z]) {
+            max = grid[r+y][c+z];
+          }
+        }
+      }*/
+
+      System.out.println(max);
+
+      max -= stomp;
+      for (int y = 0; y<3;y++) {
+        for (int z = 0; z<3; z++){
+          if (grid[r+y][c+z] > max) {
+            grid[r+y][c+z] = max;
+          }
+        }
       }
-      System.out.print("\n");
+
+      System.out.println("");
+      for (int y = 0; y < R; y++){
+        for (int x = 0; x < C; x++){
+          System.out.print(grid[y][x] + " ");
+        }
+        System.out.print("\n");
+      }
     }
 
-    int depth = 0;
+
     for (int y = 0; y < R; y++) {
       for (int x = 0; x < C; x++) {
-        grid[y][y] = E - grid[y][x];
-        if (grid[y][x] > 0) {
-            depth += grid[y][x];
+        if (grid[y][x] < E) {
+          grid[y][x] = E-grid[y][x];
+          depth += grid[y][x];
+        }else {
+          grid[y][x] = 0;
         }
       }
     }
+    System.out.println(depth);
 
     return 72 * 72 * depth;
   }
 
   public static void insertionSort(int[] data){
-      for(int i = 1; i < data.length; i++){
-        int current = data[i];
-        boolean sorted = false;
-        int idx = i-1;
-        while(!(idx < 0 || data[idx] < current)){
-          data[idx+1] = data[idx];
-          idx--;
-        }
-        data[idx+1] = current;
-      }
-    }
+     for(int i = 1; i < data.length; i++){
+       int current = data[i];
+       boolean sorted = false;
+       int idx = i-1;
+       while(!(idx < 0 || data[idx] < current)){
+         data[idx+1] = data[idx];
+         idx--;
+       }
+       data[idx+1] = current;
+     }
+   }
 
   public static void main(String[] args) throws FileNotFoundException{
-    bronze("test.txt");
+    System.out.println(bronze("test.txt"));
   }
 }
