25529167c2c2570048978f9ff559bedc7801934f
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-03-10 16:45:41 -0400

    set up readfile for silver

--- USACO.java
+++ USACO.java
@@ -90,13 +90,41 @@ public class USACO{
     return 72 * 72 * depth;
   }
 
-  public static int silver(){
-    /*fewer than two neighbouring valid spots or more than three valid spots is NO LONGER VALID
-    any valid spot with two or three live neighboursremians valid
-    any invalid cell with exactly three valid neighbours becomes a valid spot
+  public static int silver(String filename) throws FileNotFoundException{
+    /*fewer than two neighbouring valid cell or more than three valid cells dies
+    any live cell with two or three live neighbours remians live
+    any dead cell with exactly three live neighbours becomes a live cell
     you cannot go on trees
     */
+
+    File file = new File(filename);
+    Scanner in = new Scanner(file);
+    int R = in.nextInt();
+    int C = in.nextInt();
+    int T = in.nextInt();
+
+    boolean[][] map = new boolean[R][C];
+    int[][] grid = new int[R][C];
+
+    for (int r = 0; r < R; r++) {
+			String line = in.next();
+			for (int c = 0; c < C; c++) {
+				map[r][c] = (line.charAt(c) != '*');
+			}
+		}
+
+    int r_s = in.nextInt()-1;
+		int c_s = in.nextInt()-1;
+		int r_e = in.nextInt()-1;
+		int c_e = in.nextInt()-1;
+
+    for(int i = 0; i < T; i++){
+      
+    }
+
+    return 0;
   }
+
   public static void main(String[] args) throws FileNotFoundException{
     System.out.println(bronze("test.txt"));
   }
